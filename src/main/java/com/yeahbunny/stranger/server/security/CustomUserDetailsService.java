6cfe485bbd87a0e1e4ab18d6d0ae3e5976590cae
Krolis <lolosiowo2>
2017-05-30 08:21:30 +0200

    Merge branch 'dev'
    # Conflicts:
    #	src/main/java/com/yeahbunny/stranger/server/security/CustomUserDetailsService.java

--- src/main/java/com/yeahbunny/stranger/server/security/CustomUserDetailsService.java
+++ src/main/java/com/yeahbunny/stranger/server/security/CustomUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.yeahbunny.stranger.server.security;
 
+import com.yeahbunny.stranger.server.model.User;
+import com.yeahbunny.stranger.server.services.UserService;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,8 +9,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
-import com.yeahbunny.stranger.server.model.User;
-import com.yeahbunny.stranger.server.services.UserService;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class CustomUserDetailsService implements UserDetailsService {
@@ -18,12 +21,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
-    	User user = userService.findUserByUsername(login);
-    	if (user == null)
-    		throw new UsernameNotFoundException("USER_NOT_FOUND");
+        User user = userService.findUserByUsername(login);
+        if (user == null)
+            throw new UsernameNotFoundException("USER_NOT_FOUND");
 
         List<GrantedAuthority> roles = new ArrayList<>();
         roles.add(new SimpleGrantedAuthority(AppRoles.USER));
-        return new CustomUserDetails((long)1,login, "asd", roles);
+        return new CustomUserDetails(user.getIdUser(), user.getLogin(), user.getHashedPw(), roles);
     }
 }
